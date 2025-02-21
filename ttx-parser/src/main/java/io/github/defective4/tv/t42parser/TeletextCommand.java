2ff0c6b2a20c6196ee3e499311e0cf4665f0f0f6
Defective <def3ctive4@gmail.com>
2025-02-21 23:10:14 +0100

    Show ANSI progress when parsing teletext

--- ttx-parser/src/main/java/io/github/defective4/tv/t42parser/TeletextCommand.java
+++ ttx-parser/src/main/java/io/github/defective4/tv/t42parser/TeletextCommand.java
@@ -2,6 +2,7 @@ package io.github.defective4.tv.t42parser;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 public class TeletextCommand {
@@ -12,6 +13,13 @@ public class TeletextCommand {
         try (OutputStream os = proc.getOutputStream()) {
             os.write(t42data);
         }
+        try (InputStreamReader reader = new InputStreamReader(proc.getErrorStream())) {
+            while (true) {
+                int c = reader.read();
+                if (c == -1) break;
+                System.err.print((char) c);
+            }
+        } catch (Exception e) {}
         if (proc.waitFor() != 0) throw new IOException("Couldn't decode teletext");
     }
 }
